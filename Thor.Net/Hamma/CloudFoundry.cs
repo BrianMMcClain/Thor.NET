3327c583ae2b47674ba522b6d6f2880caa42f953
Adron Hall - Macbook Air/Win7 VM <adronhall@gmail.com>
2012-07-08 23:12:20 -0700

    Minor modifications.

--- Thor.Net/Hamma/CloudFoundry.cs
+++ Thor.Net/Hamma/CloudFoundry.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Thor.Net.Hamma
+﻿namespace Thor.Net.Hamma
 {
-    class CloudFoundry
+    public class CloudFoundry
     {
     }
 }
