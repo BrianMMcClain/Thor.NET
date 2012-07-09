64cee46701ceb51e7b7ecb31697a0e41b7cd00ac
Adron Hall - Macbook Air/Win7 VM <adronhall@gmail.com>
2012-07-08 18:08:47 -0700

    Added first test, that merely proves the test framework works.

---
+++ Thor.Net.Tests/TestingTesting.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Thor.Net.Tests
+{
+    [TestFixture]
+    public class TestingTesting
+    {
+        [Test]
+        public void TestingTestingFramework()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
