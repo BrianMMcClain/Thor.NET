18a7f83f93ca5672eb063fc5f8e0ebff6d95b019
Adron Hall (Win8 Dev on Mac Air) <adronhall@gmail.com>
2012-08-13 16:48:19 -0700

    Added action calls via the (bunch of pattern mublings go here)
    Added action calls via the (bunch of pattern mublings go here)

--- Ui.Wpf/ViewModels/HammaViewModel.cs
+++ Ui.Wpf/ViewModels/HammaViewModel.cs
@@ -1,7 +1,27 @@
-﻿namespace Ui.Wpf.ViewModels
-{
-    public class HammaViewModel
-    {
+﻿using Caliburn.Micro;
 
+namespace Ui.Wpf.ViewModels
+{
+    public class HammaViewModel : Conductor<object>
+    {
+        public HammaViewModel()
+        {
+            ShowFoundryClouds();
+        }
+
+        public void ShowFoundryClouds()
+        {
+            ActivateItem(new FoundryCloudsViewModel());
+        }
+
+        public void ShowFoundryApplications()
+        {
+            ActivateItem(new FoundryApplicationsViewModel());
+        }
+
+        public void ShowFoundryServices()
+        {
+            ActivateItem(new FoundryServicesViewModel());
+        } 
     }
 }
