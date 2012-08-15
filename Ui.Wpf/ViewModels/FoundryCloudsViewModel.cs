062e42e4626f7f94c847de4f7ad224397389f3ad
Adron Hall (Win8 Dev on Mac Air) <adronhall@gmail.com>
2012-08-15 09:59:55 -0700

    Added sample projects to work with, focusing on the Metro.JavaScript project now so brought it out of the secondary folder.

--- Ui.Wpf/ViewModels/FoundryCloudsViewModel.cs
+++ Ui.Wpf/ViewModels/FoundryCloudsViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Caliburn.Micro;
 
 namespace Ui.Wpf.ViewModels
 {
-    public class FoundryCloudsViewModel
+    public class FoundryCloudsViewModel : Screen
     {
+
     }
 }
