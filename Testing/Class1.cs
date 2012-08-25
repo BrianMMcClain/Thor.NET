6113be44290004bb40a7f3e050fbeb71aade6580
Adron Hall (Win 8 Dev VM) <adronhall@gmail.com>
2012-08-24 22:46:56 -0700

    Again cleaning up the projects.

--- Testing/Class1.cs
+++ Testing/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoundryGate
+namespace Testing
 {
     public class Class1
     {
--- Testing/Properties/AssemblyInfo.cs
+++ Testing/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FoundryGate")]
+[assembly: AssemblyTitle("Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FoundryGate")]
+[assembly: AssemblyProduct("Testing")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4bcff695-15c9-4c2e-a9ad-573551b6fb70")]
+[assembly: Guid("8754c20f-1d8f-4e45-a3c6-12ef994a0e4e")]
 
 // Version information for an assembly consists of the following four values:
 //
