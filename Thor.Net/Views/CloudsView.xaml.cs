257e9d1b6a63430efc1e26a040f2181c452d17d6
Adron Hall (Win x64 iMac Machine) <adronhall@gmail.com>
2012-09-08 16:39:47 -0700

    Cleanup

--- Thor.Net/Views/CloudsView.xaml.cs
+++ Thor.Net/Views/CloudsView.xaml.cs
@@ -5,6 +5,7 @@ using MahApps.Metro.Controls;
 using Thor.Asgard;
 using Thor.Asgard.Bridges;
 using Thor.Models.Jörð;
+using Thor.Net.Views.Clouds;
 
 namespace Thor.Net.Views
 {
@@ -37,31 +38,5 @@ namespace Thor.Net.Views
             CloudsListView.Visibility = Visibility.Hidden;
             CloudsAddView.Visibility = Visibility.Hidden;
         }
-
-        public List<Tile> Tiles { get; set; }
-
-        private void GetTargetTiles()
-        {
-            //var tiles = new List<Tile>();
-            //var targets = new Targets(new SettingsWrapper()).GetTargets();
-            //foreach (var target in targets)
-            //{
-            //    var tile = new Tile() { Title = target.Name };
-            //    tile.Click += TileOnClick;
-            //    tile.Margin = new Thickness(15, 15, 0, 0);
-
-            //    CloudsViewStackPanel.Children.Add(tile);
-            //    tiles.Add(tile);
-            //}
-
-            //Tiles = tiles;
-        }
-
-        private void TileOnClick(object sender, RoutedEventArgs routedEventArgs)
-        {
-            var tile = routedEventArgs.Source as Tile;
-            MessageBox.Show(routedEventArgs.OriginalSource + "\n" + tile.Title);
-        }
-
     }
 }
