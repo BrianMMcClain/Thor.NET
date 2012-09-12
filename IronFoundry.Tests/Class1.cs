10c03157db9bf6f891590a1d7b7459b8a163c60c
Adron Hall - Macbook Air/Win7 VM <adronhall@gmail.com>
2012-09-11 18:16:14 -0700

    Added a fakish test for testing the Iron Foundry Code base.

---
+++ IronFoundry.Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using IronFoundry.Vcap;
+using NUnit.Framework;
+
+namespace IronFoundry.Tests
+{
+    [TestFixture]
+    public class where_vcap_client_is_used
+    {
+        [TestFixtureSetUp]
+        public void when_verifying_cloud_target()
+        {
+         
+        }
+
+
+        [Test]
+        public void Blagh()
+        {
+            // Just figuring it out, don't look at this test.
+            VcapClient client = new VcapClient("http://api.robotech.wa1.wfabric.com");
+            client.Login("adron.hall@tier3.com", "77stumptown!");
+            var clientInfo = client.GetInfo();
+            clientInfo.User.Should().NotBeNull();
+        }
+    }
+}
