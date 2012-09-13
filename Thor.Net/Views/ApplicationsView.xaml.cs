c804cd7e064dafcf7e97328b22a5ec634be87267
Adron Hall - Macbook Air/Win7 VM <adronhall@gmail.com>
2012-09-12 19:10:48 -0700

    Cleanup some code.

--- Thor.Net/Views/ApplicationsView.xaml.cs
+++ Thor.Net/Views/ApplicationsView.xaml.cs
@@ -3,9 +3,6 @@ using Thor.Net.Views.Applications;
 
 namespace Thor.Net.Views
 {
-    /// <summary>
-    /// Interaction logic for ApplicationsView.xaml
-    /// </summary>
     public partial class ApplicationsView : UserControl
     {
         public ApplicationsView()
